52b8a6c80d083c72a0725a712aecc74b28fd7a6c
Amanda Souza <nanda.hamandadesouza@gmail.com>
2021-10-08 10:38:08 -0300

    Feat. Adicionando exceções na api

--- sistemaparaescola/APIESCOLA/controllers/seriesControllers.js
+++ sistemaparaescola/APIESCOLA/controllers/seriesControllers.js
@@ -4,9 +4,19 @@ const newSeries = async (req, res) => {
         try{
             var teacher = req.body.teacher
             var school_Grade = req.body.school_Grade
-            const series = await Series.create({teacher:teacher,
-            school_Grade: school_Grade})
-            res.status(201).send({series})
+            
+            
+            if(teacher == "" && school_Grade == ""){
+                res.status(400).send('Os campos estão vazios')
+            }else if(teacher == undefined || teacher == ""){
+                res.status(400).send('Professor esta vazio')             
+            }else if(school_Grade == undefined || school_Grade == ""){
+                res.status(400).send('Serie esta vazio')
+            }else{
+                    const series = await Series.create({teacher:teacher,
+                    school_Grade: school_Grade})
+                    res.status(201).send({series})
+            }
 
         }catch(error){
             res.status(400).send(error)
@@ -31,23 +41,48 @@ const updateSeries = async (req, res) => {
         var upTeacher = req.body.teacher
         var upSchoolGrade = req.body.school_Grade
 
-        const upSeries = await Series.findByPk(id);
-        upSeries.teacher = upTeacher
-        upSeries.school_Grade = upSchoolGrade
+        if(upTeacher =="" && upSchoolGrade == ""){
+            res.status(400).send('Os campos estão vazio')            
+        }else if(upTeacher == undefined || upTeacher == ""){
+            res.status(400).send('Campo professor esta vazio')         
+        }else if(upSchoolGrade == undefined || upSchoolGrade == ""){
+            res.status(400).send('Campo serie esta vazio')         
+        }else{
 
-        const edit = await upSeries.save()
-        
-        res.status(200).send(edit)
-        
+            const upSeries = await Series.findByPk(id);
+            upSeries.teacher = upTeacher
+            upSeries.school_Grade = upSchoolGrade
+
+            const edit = await upSeries.save()
+            
+            res.status(200).send(edit)
+
+        }
+
     } catch (error) {
         res.status(400).send(error)
 
     }
 }
 
+const deleteSerie = async (req, res) => {
+    try {
+        var id = req.params.id
+
+        const deSerie = await Series.findByPk(id);
+        deSerie.destroy()
+        res.status(200).send('Excluido')
+
+        
+    } catch (error) {
+        res.status(400).send(error)
+       
+    }
+}
 
 
-module.exports = {newSeries, allSeries, updateSeries}
+
+module.exports = {newSeries, allSeries, updateSeries, deleteSerie}
 
 // router.get('/series', (req, res) => {
 //     Series.findAll().then(series => {
--- sistemaparaescola/APIESCOLA/router.js
+++ sistemaparaescola/APIESCOLA/router.js
@@ -8,6 +8,8 @@ const seriesControllers = require('./controllers/seriesControllers')
 router.post('/serie', seriesControllers.newSeries)
 router.get('/series', seriesControllers.allSeries)
 router.put('/serie/:id', seriesControllers.updateSeries)
+router.delete('/serie/:id', seriesControllers.deleteSerie)
+
 
 
 module.exports = router 
